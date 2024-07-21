540598f4087b7ebb0cc259e26f847fe395186f6b
Luca Vivona <lucavivona01@gmail.com>
2024-07-20 20:13:00 -0400

    Add Tailwind CSS animations and utilities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,9 +15,42 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        pad: {
+          '0%': { padding: '0' },
+          '50%': { padding: '0' },
+          '100%': { padding: '5rem' }, // Adjust to your desired padding value
+        },
+        translateUp: {
+          '0%': { transform: 'translateY(20%)', opacity : "0%" },
+          '100%': { transform: 'translateY(0)', opacity : "100%" },
+        },
+      },
+      animation: {
+        translateUp: 'translateUp 1.5s ease-out',
+        pad: 'pad 1.5s ease-in-out forwards', // 1s animation duration, forwards to keep the final state
+      },
     },
   },
-  plugins: [addVariablesForColors],
+  plugins: [addVariablesForColors,
+    function ({ addUtilities } : { addUtilities : any }) {
+      addUtilities({
+        '.no-scrollbar': {
+          /* Hide scrollbar for Chrome, Safari, and Opera */
+          '-webkit-overflow-scrolling': 'touch',
+          '-webkit-scrollbar': {
+            display: 'none',
+          },
+          /* Hide scrollbar for IE, Edge, and Firefox */
+          '-ms-overflow-style': 'none', /* IE and Edge */
+          'scrollbar-width': 'none', /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none',
+        },
+      })
+    },
+  ],
 };
 
 function addVariablesForColors({ addBase, theme }: any) {
